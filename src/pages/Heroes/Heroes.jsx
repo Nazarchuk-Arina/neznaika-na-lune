c62890913fd2870e870de861e5396e62db8d3f21
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2025-01-28 00:11:06 +0200

    fff

--- src/pages/Heroes/Heroes.jsx
+++ src/pages/Heroes/Heroes.jsx
@@ -3,6 +3,7 @@ import HeroesList from "../../components/HeroesList/HeroesList";
 import StarryBackground from "../../components/StarryBackground/StarryBackground";
 import { data } from "../../assets/data/heroes.js";
 import Header from "../../components/Header/Header.jsx";
+import Gif from "../../components/Gif/Gif.jsx";
 
 const Heroes = () => {
   return (
@@ -11,6 +12,7 @@ const Heroes = () => {
       <div style={{ position: "relative", zIndex: 1 }}>
         <Header />
         <HeroesList heroes={data} />
+        <Gif />
         <Footer />
       </div>
     </div>
